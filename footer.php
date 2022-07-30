6e55ba88dc05f39b392a218faa438e455d904639
dinesh-404 <khuntidnsh@gmail.com>
2022-07-30 10:03:01 +0530

    footer

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,15 +8,22 @@
     <title>footer</title>
     <link rel="stylesheet" href="testdesign.css">
 </head>
+
 <body>
     <div class="footer">
-        &copy;2022 all rights reserved
-        developed by dinesh
-        <img src="NavbarIcons\fbicon.png" alt="" srcset="">
-        
-        <img src="NavbarIcons\linkedinicon.png" alt="" srcset="">
-        <img src="NavbarIcons\twittericon.png" alt="" srcset="">
-        <img src="NavbarIcons\instaicon.png" alt="" srcset="">
+        <div class="footerleft">
+
+            &copy;2022 all rights reserved<br>
+            developed by dinesh
+        </div>
+        <div class="footerright">
+
+            <img src="NavbarIcons\fbicon.png" alt="" srcset="">
+            <img src="NavbarIcons\linkedinicon.png" alt="" srcset="">
+            <img src="NavbarIcons\twittericon.png" alt="" srcset="">
+            <img src="NavbarIcons\instaicon.png" alt="" srcset="">
+        </div>
     </div>
 </body>
+
 </html>
