7c3fec7a9b842914b70402dcbbfbad3147511dc4
dinesh-404 <khuntidnsh@gmail.com>
2022-08-17 21:01:05 +0530

    doing profile textfields

--- profile.php
+++ profile.php
@@ -41,65 +41,84 @@ if (isset($_SESSION['uid'])) {
             </div>
             <button type="button" class="button" onclick='scrollupdate()'>View Profile</button>
         </div>
-        <fieldset id="fieldset">
-            <legend id="legend">Profile</legend>
-            <div class="ProfileWrapper">
-                <div class="left">
+        <div class="ProfileWrapper">
+            <div class="left">
 
+                <div class="txtfieldwrapper">
                     <label for='fullname'>Full Name</label>
                     <input type='text' class='fields' name='fullname' value='<?php echo $row['fullname']; ?>' id='fullname' disabled required><br>
+                </div>
+                <div class="txtfieldwrapper">
 
                     <label for='age'>Age</label>
                     <input type='text' class='fields' name='age' value='<?php echo $row['age']; ?>' id='age' disabled required><br>
+                </div>
+                <div class="txtfieldwrapper">
 
                     <label for='gender'>Gender</label>
                     <input type='text' class='fields' name='gender' value='<?php echo $row['gender']; ?>' id='gender' disabled required><br>
+                </div>
+                <div class="txtfieldwrapper">
 
                     <label for='address'>address</label>
                     <input type='text' class='fields' name='address' value='<?php echo $row['address']; ?>' id='address' disabled required><br>
+                </div>
+                <div class="txtfieldwrapper">
 
                     <label for='username'>Username</label>
                     <input type='text' class='fields' name='username' value='<?php echo $row['username']; ?>' id='username' disabled required><br>
+                </div>
+                <div class="txtfieldwrapper">
 
                     <label for='password'>Password</label>
                     <input type='text' class='fields' name='password' value='<?php echo $row['password']; ?>' id='password' disabled required><br>
+                </div>
+                <div class="txtfieldwrapper">
 
                     <label for='email'>Email</label>
                     <input type='text' class='fields' name='email' value='<?php echo $row['email']; ?>' id='email' disabled required><br>
+                </div>
+                <div class="txtfieldwrapper">
 
                     <label for='mobileno'>Mobile No</label>
                     <input type='text' class='fields' name='mobileno' value='<?php echo $row['mobile']; ?>' id='mobileno' disabled required><br>
-
-                    <input type="button" value="update" id="updatebtn" class="button">
-                    <input type="button" value="submit" onclick="validate()" class="button">
                 </div>
-                <div class="right">
-                    <?php
+     
 
-                    if (strlen($row['image']) == 0) {
-                    ?>
-                        <input type="file" name="avatar" class='fields' accept="image/*" disabled id=""><br>
-                    <?php
-                    } else {
-                        echo "<img src='$imgpath' alt='profilepic' class='profilepic'><br>";
-                    }
-                    ?>
-                </div>
+                    <!-- <input type="button" value="update" id="updatebtn" onclick="enable()" class="button">
+                    <input type="button" value="submit" onclick="validate()" class="button"> -->
+                
             </div>
-        </fieldset>
-
-        <div class="container">
-            <labfullnameel for="profilepic">Profile Picture</label>
+            <div class="right">
                 <?php
 
                 if (strlen($row['image']) == 0) {
                 ?>
-                    <input type="file" name="avatar" class='fields' accept="image/*" disabled id=""><br>
+                    <input type="file" name="avatar" class='fields' accept="image/*" disabled id="" style="display: none;"><br>
                 <?php
                 } else {
-                    echo "<img src='$imgpath' alt='profilepic' class='profilepic'><br>";
+                    echo "<img src='$imgpath' alt='profilepic' class='profilepic' ondblclick='filebrowse()'><br>";
                 }
                 ?>
+                <input type="file" name="file" id="filebrowse" style="display: none;">
+                <input type="button" value="update" id="updatebtn" onclick="enable()" class="button">
+                    <input type="button" value="submit" onclick="validate()" class="button">
+            </div>
+        </div>
+
+        <!-- <div class="container">
+                <label for="profilepic">Profile Picture</label>
+                <?php
+
+                // if (strlen($row['image']) == 0) {
+                // 
+                ?>
+                //     <input type="file" name="avatar" class='fields' accept="image/*" disabled id=""><br>
+                // <?php
+                    // } else {
+                    //     echo "<img src='$imgpath' alt='profilepic' class='profilepic'><br>";
+                    // }
+                    ?>
 
                 <label for='fullname'>Full Name</label>
                 <input type='text' class='fields' name='fullname' value='<?php echo $row['fullname']; ?>' id='fullname' disabled required><br>
@@ -127,11 +146,10 @@ if (isset($_SESSION['uid'])) {
 
                 <input type="button" value="update" id="updatebtn" class="button">
                 <input type="button" value="submit" onclick="validate()" class="button">
-        </div>
+            </div> -->
     </form>
-    <div class="crudbtn">
-    </div>
     <script src="update.js"></script>
+    <br><br>
     <?php include('footer.php'); ?>
 </body>
 
--- update.js
+++ update.js
@@ -1,43 +1,55 @@
 let update = document.getElementById('updatebtn');
 let fields = document.getElementsByClassName('fields');
 let form = document.getElementById('updateform');
-update.addEventListener('click', function () {
-    for (let i = 0; i < fields.length; i++) {
-        fields[i].disabled = false;
+var en = 0;
+function enable() {
+    
+    if (en == 0) {
+        for (let i = 0; i < fields.length; i++) {
+            fields[i].disabled = false;
+        }
+        en++;
     }
-    console.log(fields);
-})
+    else{
+        for (let i = 0; i < fields.length; i++) {
+            fields[i].disabled = true;
+        }
+        en=0;
+    }
+
+    console.log(en);
+}
 function validate() {
     let validate = 0;
     for (let i = 0; i < fields.length; i++) {
         if (fields[i].value == "") {
-            fields[i].style.border="2px solid red";
+            fields[i].style.border = "2px solid red";
             validate++;
         }
     }
     var age = document.getElementById('age');
     if (isNaN(age.value) || age.value < 1 || age.value > 100) {
         console.log("ok");
-        age.style.border="2px solid red";
+        age.style.border = "2px solid red";
         validate++;
     }
     var mail = document.getElementById('email').value;
     var at = mail.search('@');
     var dot = mail.search(/\./);
     if (at < 2 || dot < 2 || mail == "") {
-        document.getElementById('email').style.border="2px solid red";
+        document.getElementById('email').style.border = "2px solid red";
         validate++;
     }
     var pass = document.getElementById('password').value;
     if (!pass.length > 7 || !/[0-9]/.test(pass) || !/[a-z]/.test(pass)) {
         console.log('password bitchheshshshshsh');
-        document.getElementById('password').style.border="2px solid red";
+        document.getElementById('password').style.border = "2px solid red";
         validate++;
     }
     var gender = document.getElementById('gender');
     var lower = gender.value.toLowerCase();
     if (lower != "male" && lower != "female") {
-        document.getElementById('gender').style.border="2px solid red"
+        document.getElementById('gender').style.border = "2px solid red"
         validate++;
     }
 
@@ -48,10 +60,24 @@ function validate() {
         return;
     }
 }
-function scrollupdate(){
+function scrollupdate() {
     window.scrollTo({
-        behavior:'smooth',
-        top:760,
-        left:0
+        behavior: 'smooth',
+        top: 760,
+        left: 0
     });
+}
+function filebrowse() {
+    document.getElementById('filebrowse').click();
+}
+
+let wrapper = document.getElementsByClassName("txtfieldwrapper");
+for (let i = 0; i < fields.length; i++) {
+    fields[i].addEventListener("focusin",function(){
+        wrapper[i].style.boxShadow="rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px";
+    })
+    fields[i].addEventListener("focusout",function(){
+        wrapper[i].style.boxShadow="rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px";
+    })
+   
 }
