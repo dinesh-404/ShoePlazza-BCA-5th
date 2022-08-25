a4f419aa1dc0867e677cc96ae56358756a77cb73
dinesh-404 <khuntidnsh@gmail.com>
2022-08-25 18:08:32 +0530

    fixed footer

--- footer.php
+++ footer.php
@@ -14,11 +14,12 @@
     </div>
 
     <div class="FooterRight">
-
+<div class="img">
         <img src="/ShoePlazza/NavbarIcons/fbicon.png" alt="" srcset="">
         <img src="/ShoePlazza/NavbarIcons/linkedinicon.png" alt="" srcset="">
         <img src="/ShoePlazza/NavbarIcons/twittericon.png" alt="" srcset="">
         <img src="/ShoePlazza/NavbarIcons/instaicon.png" alt="" srcset="">
+</div>
     </div>
     <div class="FooterCopy">
 
