a2dfc81114d8fbbcbbb7fd720e0f338098dd6ce3
dinesh-404 <khuntidnsh@gmail.com>
2022-08-28 21:27:49 +0530

    added brand logos and cards done almost

--- index.php
+++ index.php
@@ -24,8 +24,8 @@
     <div class="explore">
         <div class="cards">
             <div class="cardinfo">
-                <h1>Jordans By Nike </h1>
-                <img src="products/productimg/airjordan7/airjordan7-1.jpg" alt="" srcset="">
+                <img src="images/jordan.png" class="cardlogo" alt="">
+                <img src="products/productimg/airjordan7/airjordan7-1.jpg" class="pimage" alt="" srcset="">
                     <div class="txt">
                        <h2> Jordan Air 7</h2>
                         <button type="button" class="button">View More</button>
@@ -34,16 +34,18 @@
         </div>
         <div class="cards">
             <div class="cardinfo">
-                <img src="products/productimg/airjordan7/airjordan7-1.jpg" alt="" srcset="">
+                <img src="products/productimg/airjordan7/airjordan7-1.jpg"  class="pimage"alt="" srcset="">
                 <div class="txt">
                     <!-- New Collection from addidas<br> -->
                     <button type="button" class="button">View More</button>
                 </div>
             </div>
         </div>
-
     </div>
-
+<div class="panel">
+    <img src="images/nike.png" class="nike" alt="" srcset="">
+    <button class="button">Shop now</button>
+</div>
     <?php include('footer.php'); ?>
 
     <script src="cursor.js"></script>
