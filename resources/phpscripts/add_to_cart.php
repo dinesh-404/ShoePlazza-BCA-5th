aef8a9ab5d5d75e375e0700ec8be158cb82ee6e0
dinesh-404 <khuntidnsh@gmail.com>
2022-09-16 18:05:41 +0530

    minor fixes in add to cart

--- resources/phpscripts/add_to_cart.php
+++ resources/phpscripts/add_to_cart.php
@@ -8,6 +8,6 @@
         $user_id = $_SESSION['uid'];
         $cmd = "insert into user_cart (item_id,user_id,status) values ($cart_id,$user_id,'added to cart')";
         $exe = mysqli_query($connect,$cmd);
-        header('location:../../productinfo.php?pid='.$cart_id.'?err="succsessfully added to cart"');
+        header('location:../../productinfo.php?pid='.$cart_id.'&err="succsessfully added to cart"');
     }
 ?>
